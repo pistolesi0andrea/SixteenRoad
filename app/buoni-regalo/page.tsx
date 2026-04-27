c5f5e9f843b06acfc0e549e9b8768f93f6a893fe
pistolesi0andrea <pistolesi.andrea@outlook.it>
2026-04-27 17:44:49 +0200

    update buono
    update buono

--- app/buoni-regalo/page.tsx
+++ app/buoni-regalo/page.tsx
@@ -1,6 +1,7 @@
 import { getGiftCardProduct } from "@/lib/shopify";
 import { GiftCardBuilder } from "@/components/GiftCardBuilder";
 
+export const dynamic = "force-dynamic";
 export const revalidate = 10;
 
 export default async function GiftCardPage() {
